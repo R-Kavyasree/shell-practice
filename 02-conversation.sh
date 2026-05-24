062d6c88a031093420443e57292eddb6a06b2b01
R-Kavyasree <kavyasree4221@gmail.com>
2026-05-24 21:04:33 +0530

    hello world

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Trump: hey Tran, we are going to blast you !"
+echo "IRAN: YES, Please. you are welcome"
+echo "TRUMP: hey, Iran im serious"
+echo "IRAN : YES, We are also serious"

8e366dee46ccd25acaaa27822c40ce5e75f017fa
R-Kavyasree <kavyasree4221@gmail.com>
2026-05-25 21:39:49 +0530

    Hello world

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1: hey $person2, we are going to blast you !"
+echo "$person2: YES $person1, Please. you are welcome"
+echo "$person1: hey, $person2 im serious"
+echo "$person2: YES, We are also serious"s

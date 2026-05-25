8373c385ef5efa293e0d6ce26f8140aa1f6a872e
R-Kavyasree <kavyasree4221@gmail.com>
2026-05-25 21:56:35 +0530

    Hello world

--- 03-variables.sh
+++ 03-variables.sh
@@ -7,3 +7,12 @@ echo "$PERSON1: hey $PERSON2, we are going to blast you !"
 echo "$PERSON2: YES , Please. you are welcome"
 echo "$PERSON1: hey, $PERSON2 im serious"
 echo "$PERSON2: YES, We are also serious"
+
+
+echo "please enter username::"
+read USER_NAME #here USER_NAME IS VARIABLE, whaterver you enter in terminal stores in this variable
+
+echo "please enter you password::"
+read -s PASSWORD
+
+echo "password is :$PASSWORD"

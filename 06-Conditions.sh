0d3f18446bff83808a9ada3ef60c8d715c3919a2
R-Kavyasree <kavyasree4221@gmail.com>
2026-06-20 20:12:29 +0530

    hello world

--- 06-Conditions.sh
+++ 06-Conditions.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 Number=$1
 
-if [ $Number ge 20 ]; then
+if [ "$Number" -ge 20 ]; then
     echo "Given number $Number is greater than or equal to 20"
+else
+    echo "Given number $Number is less than 20"
 fi

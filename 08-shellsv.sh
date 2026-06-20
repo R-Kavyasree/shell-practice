f50a113210f2e9e7b4b49797831d990f13e23302
R-Kavyasree <kavyasree4221@gmail.com>
2026-06-20 16:54:03 +0530

    hello world

--- 08-shellsv.sh
+++ 08-shellsv.sh
@@ -3,3 +3,6 @@
 echo "All variables passed to script: $@"
 echo "All variables passed to script: $#"
 echo "this is special variables session"
+echo " First variable :$1"
+echo " second variable :$2"
+echo "script name :$0"

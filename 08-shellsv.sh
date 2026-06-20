faebf2f557fe5d583a985ce26ec84458f6bc71fa
R-Kavyasree <kavyasree4221@gmail.com>
2026-06-20 17:19:50 +0530

    hello world

--- 08-shellsv.sh
+++ 08-shellsv.sh
@@ -6,15 +6,14 @@ echo "this is special variables session"
 echo " First variable :$1"
 echo " second variable :$2"
 echo "script name :$0"
-echo "who is running ; $user"
-echo "which directory : $pwd"
-echo "Home directory: $Home"
+echo "who is running:$user"
+echo "which directory :$pwd"
+echo "Home directory:$Home"
 echo "PID of the current running script : $$"
 echo 
 sleep 5 &
 echo "PID Of background command running :$!"
 wait $!
- echo :line number : $lineNo"
- echo " Random number : $Random"
+ echo :line number $lineNo"
+ echo "Random number $Random"
  echo $?
- 

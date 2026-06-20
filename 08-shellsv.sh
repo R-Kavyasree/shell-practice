b9d4c185baea306a434ec40e15485185bab63fb4
R-Kavyasree <kavyasree4221@gmail.com>
2026-06-20 17:32:22 +0530

    hello world

--- 08-shellsv.sh
+++ 08-shellsv.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-echo "All variables passed to script: $@"
-echo "Number of variables passed: $#"
-echo "First variable: $1"
-echo "Script name: $0"
-echo "Who is running this: $USER"
-echo "Which directory: $PWD"
-echo "Home directory: $HOME"
-echo "PID of the current script: $$"
-sleep 5 &
-echo "PID of the background command running just now: $!"
+echo "All variables passed to script :$@"
+echo "New of variables passed : $#"
+echo "first variable :$1"
+echo "Second variable :$2"
+echo "script name: $0"
+echo "who is running this : $USER"
+echo " which directory :$PWD"
+echo "Home directory : $HOME"
+echo "PID of the current script : $$"
+sleep 7 &
+echo "PID of the background command running just now : $!"
 wait $!
-echo "Line number: $LINENO"
-echo "Script executed in $SECONDS seconds"
+echo "Line number :$LINENO"
+echo "script executed in $SECONDS seconds"
 echo "Random number: $RANDOM"

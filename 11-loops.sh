68eb2f2fcd3709c216d45d0f0a75b3e3dc205af4
R-Kavyasree <kavyasree4221@gmail.com>
2026-06-25 15:11:44 +0530

    Hello World

---
+++ 11-loops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID =$(id -u)
+LOGS_DIR=/var/logs/shell-script
+LOGS_FILE="#LOGS_DIR/$0.log"
+
+if [ $USERID -ne 0 ]; then
+  echo "Please run this script with root access"
+  exit 1
+  fi
+
+  VALIDATE()
+{
+    if [ $2 -ne 0 ]; then
+    echo "Installing &1 is ....Failed"
+    exit 1
+    else
+    echo "Installing $1 is ..... success"
+    fi
+    
+    }
+
+    for package in $E@
+
+    do 
+       echo "Intalling $package"
+       dnf list lintalled $package
+      if [ $? -ne 0 ];then
+        dnf install package -yy &>> $LOGS_FILE
+        VALIDATE "Installing $package" $?
+
+        else 
+        echo "$package is already installed....skipping"
+        fi
+        
+        done
